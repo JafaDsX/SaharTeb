9684d5d6dfb2c8be375492cebd5146c8d9cffee4
jafadsx <jafadsx@gmail.com>
2026-02-09 16:30:49 +0330

    Finish core app (without HTML templates)

---
+++ saharteb/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from core.views import HomeView, AboutUsView, ContactUsView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path("about-us/", AboutUsView.as_view(), name="about_us"),
+    path("contact-us/", ContactUsView.as_view(), name="contact_us"),
+]
--- saharteb/core/views.py
+++ saharteb/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, "core/home.html")
+
+class AboutUsView(View):
+    def get(self, request):
+        return render(request, "core/about.html")
+
+class ContactUsView(View):
+    def get(self, request):
+        return render(request, "core/contact.html")
+
