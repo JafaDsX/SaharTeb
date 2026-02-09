e2dfee48bc8723cb0923806f81b19dbe7dbb0ed4
jafadsx <jafadsx@gmail.com>
2026-02-09 16:04:20 +0330

    start project and configuration

---
+++ saharteb/saharteb/local_settings.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'django-insecure-9p8^hx5$q21x-=jwu)ie6e&kc=i_=7^+!@ar3c^03$8wo98@)&'
+
+DEBUG = True
+
+
+DB_NAME='mydb'
+DB_USER='saharuser2025'
+PASSWORD='S:DLKFJ()*4oijsodfw234'
+HOST='localhost'
+PORT='5432'
+
+SUPER_USER='mohammadjafa'
+SUPER_PASSWORD='SDLFKj3049oqpdsl'
